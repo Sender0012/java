acda6a3fb7426b96cf0a3de4885c14bcb93ac7d7
sender <senderowski.jakub1@gmail.com>
2019-12-12 17:59:08 +0100

    SECOND SENDER2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Sender");
+	// write your code here
+	    System.out.println("Sender");
         System.out.println("SENDER2");
-        System.out.println("Sender3");
-        System.out.println("Senderus");
     }
 }
