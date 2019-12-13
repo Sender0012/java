6e7ab5311cdfe7d92de78807337a24e48af6c5d7
sender <senderowski.jakub1@gmail.com>
2019-12-14 00:05:16 +0100

    new line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-	    System.out.println("Sender");
+        // write your code here
+
+        System.out.println("Sender");
         System.out.println("SENDER2");
+        System.out.println("SENDER3");
     }
 }
